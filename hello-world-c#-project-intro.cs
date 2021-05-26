118abbcf1c7d631bc45b61f817a4c697b2121ff3
Prannvat Singh <prannvat2005@gmail.com>
2021-05-26 13:20:48 +0100

    My first c# intro project! Simple printing of 'hello world'.

---
+++ hello-world-c#-project-intro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+
+namespace HelloWorld_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+        }
+    }
+}
