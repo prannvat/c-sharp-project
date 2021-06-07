46ecf9835d92dc14b0e067f177bacafdefaa6c1d
Prannvat Singh <prannvat2005@gmail.com>
2021-06-07 13:22:50 +0100

    Learning classes, this is the program class

---
+++ classes-program.cs
@@ -0,0 +1,30 @@
+﻿
+using classes.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prannvat = new Person();
+            prannvat.FirstName = "Prannvat";
+            prannvat.LastName = "Singh";
+            prannvat.Introduce();
+            //you want one class per file or it can get too chaotic
+            //so you move each file to separate files.
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+            //USING THE CALCULATOR CLASS.
+
+        }
+
+    }
+}
