854e44d7ff078837b07cf646dd4bf0fce15bec5f
Prannvat Singh <prannvat2005@gmail.com>
2021-06-07 13:27:23 +0100

    This is the calculator class, that is called in the main program class.

---
+++ classes-calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes.Math 
+    //different functions and methods should be in different namsepaces, 
+    //according to their function, henec this says class.Math
+
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+
+        }
+    }
+}
+ 
