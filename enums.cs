dfe2206296124f1acf5afad297d0c70e2d3724a2
Prannvat Singh <prannvat2005@gmail.com>
2021-06-11 13:01:59 +0100

    Learning enums and some parsing.

---
+++ enums.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enums
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1, 
+        RegisiteredAirMail = 2,
+        Express = 3
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+            //you can use casting to convert an integer to the enum,
+            //in  this case the 'ShippingMethod'
+            //in this case the value 'Express' is returned.
+
+            Console.WriteLine(method.ToString());
+            //the above converts the enum into a string, so the method is converted into string.
+            //this returns 'Express' and it is a string.
+            //Console.WriteLine always calls the .ToString method so you don't need to type it,
+            //while using Console.WriteLine.
+
+            var methodName = "Express";
+            //Parsing is what you call converting a string into a different data type.
+            //Here we are going to parse the string into a ShippingMethod.
+           var shippingMethod =  (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName); 
+
+        }
+    }
+}
