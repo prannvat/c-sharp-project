5281e49b2cbc52ad07b8c3030ca83d8d006832b9
Prannvat Singh <prannvat2005@gmail.com>
2021-07-26 15:44:18 +0100

    A very small example of where procedural programming is used to separate code, keep everything understandeable and neat

---
+++ procedural-programming.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace procedural_programming
+{
+     internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? ");
+            var userName = Console.ReadLine();
+            var reversed = ReverseName(userName);
+            Console.WriteLine("Reversed name: " + reversed);
+
+            
+        }
+
+        public static string ReverseName(string userName)
+        {
+            var array = new char[userName.Length];
+            for (var i = userName.Length; i > 0; i--)
+                array[userName.Length - i] = userName[i - 1];
+
+            return new string(array);
+            
+        }
+    }
+}
