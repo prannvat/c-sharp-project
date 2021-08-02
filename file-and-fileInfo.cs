4dbbc80ffa82005ae1d0b97fe4135dd5b45e023f
Prannvat Singh <prannvat2005@gmail.com>
2021-08-02 14:21:33 +0100

    Applying knowledge about file and fileInfo

---
+++ file-and-fileInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace file_and_fileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+            File.Copy(@"C:\temp\myfilw.jpg","d:\temp\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var contents = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
+            fileInfo.OpenRead();
+                
+                //Everything that I have done before is pretty simple and shows that file and fileInfo are actually very similar. The reasons for their seperate usage is given in the "system.IO-namespace" file.
+        }
+    }
+}
