3fe4dd0c417cd3ab31dc15675841d4f865e9eebb
Prannvat Singh <prannvat2005@gmail.com>
2021-08-04 15:44:30 +0100

    Applying knowledge about directory and directoryInfo into examples

---
+++ directory-and-directoryInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace directory_and_directoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\Users\temp\folder1");
+
+            var files = Directory.GetFiles(@"c:\projects\c-sharp-project", "*.sln", SearchOption.AllDirectories);
+            foreach (var file in files)
+                Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\projects\c-sharp-project", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+
+
+        }
+    }
+}
